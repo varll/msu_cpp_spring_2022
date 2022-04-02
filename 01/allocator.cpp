c9c7c6b2da5f4579b344ec2e375c4693be64f437
varll <93476866+varll@users.noreply.github.com>
2022-04-02 19:18:52 +0300

    Add files via upload

--- 01/allocator.cpp
+++ 01/allocator.cpp
@@ -1,35 +1,35 @@
 #include "allocator.h"
 
-    void Allocator::makeAllocator(size_t maxSize)
+void Allocator::makeAllocator(size_t maxSize)
+{
+    if (max_size != 0)
     {
-        if (max_size != 0)
-        {
-            max_size = 0;
-            offset = 0;
-            delete[] pointer;
-        }
-        max_size = maxSize;
-        pointer = new char[max_size];
+        max_size = 0;
         offset = 0;
-    }
-
-    char* Allocator::alloc(size_t size)
-    {
-        if (offset + size > max_size)
-            return nullptr;
-
-        char* result = pointer;
-        result += offset;
-        offset += size;
-        return result;
-    }
-
-    void Allocator::reset()
-    {
-        offset = 0;
-    }
-
-    Allocator::~Allocator()
-    {
         delete[] pointer;
     }
+    max_size = maxSize;
+    pointer = new char[max_size];
+    offset = 0;
+}
+
+char* Allocator::alloc(size_t size)
+{
+    if (offset + size > max_size)
+        return nullptr;
+
+    char* result = pointer;
+    result += offset;
+    offset += size;
+    return result;
+}
+
+void Allocator::reset()
+{
+    offset = 0;
+}
+
+Allocator::~Allocator()
+{
+    delete[] pointer;
+}
